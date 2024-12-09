b86e26dd70c1e489f3d0d5366041bb5e221c07e8
Ievgen Pyrogov <iamexile@gmail.com>
2024-12-09 20:54:59 +0100

    Initial

---
+++ lib/pdfium.ex
@@ -0,0 +1,18 @@
+defmodule Pdfium do
+  @moduledoc """
+  Documentation for `Pdfium`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pdfium.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pdfium_test.exs
@@ -0,0 +1,8 @@
+defmodule PdfiumTest do
+  use ExUnit.Case
+  doctest Pdfium
+
+  test "greets the world" do
+    assert Pdfium.hello() == :world
+  end
+end
