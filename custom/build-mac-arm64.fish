27f251ddf3556766523cadd2b5c7f71a34be4379
Ievgen Pyrogov <iamexile@gmail.com>
2024-12-12 19:35:26 +0100

    custom build script

--- custom/build-mac-arm64.fish
+++ custom/build-mac-arm64.fish
@@ -1,19 +1,17 @@
 set -ex
 
-# inputs for the script: x86 or arm64
-# inputs for linux script: x86 or arm64
-# inputs for linux-musl script: x86 or arm64
+# TODO: script to update builds.json
 
-# TODO: script to update variables.csv
+os=$1   # mac, linux, linux-musl
+arch=$2 # arm64, amd64
+otp=$3  # otp27.2, otp25.2
 
-IFS=',' read -r \
-    arch \
-    pdfium_download_link \
-    pdfium_sha256sum \
-    otp_download_link \
-    otp_sha256sum \
-    output_name \
-    <<< $(cat variables.csv | sed -n 2p)
+eval $(jq -r --arg os "$os" \
+           --arg arch "$arch" \
+           --arg otp "$otp" \
+           '.builds[] | 
+            select(.os == $os and .arch == $arch and .otp == $otp) |
+            to_entries | .[] | "\(.key)=\(.value)"' builds.json)
 
 otp_directory_name=$(basename $otp_download_link .tar.gz)
 otp_archive_name=$(basename $otp_download_link)
@@ -25,33 +23,27 @@ output_directory_name="pdfium-mac-arm64-output"
 
 # 1. Clean-up
 
-rm -fr /tmp/$otp_directory_name
-rm -fr /tmp/$otp_archive_name
-rm -fr /tmp/$pdfium_directory_name
-rm -fr /tmp/$pdfium_archive_name
-rm -fr /tmp/$output_directory_name
+rm -fr $otp_directory_name
+rm -fr $otp_archive_name
+rm -fr $pdfium_directory_name
+rm -fr $pdfium_archive_name
+rm -fr $output_directory_name
 
-mkdir /tmp/$otp_directory_name
-mkdir /tmp/$pdfium_directory_name
-mkdir /tmp/$output_directory_name
+mkdir $otp_directory_name
+mkdir $pdfium_directory_name
+mkdir $output_directory_name
 
-# 2. Prepare
+# 2. Download Erlang
 
-pushd /tmp
-
-# 2.1 Download Erlang
-
-wget --quiet --directory-prefix /tmp $otp_download_link
+wget --quiet $otp_download_link
 echo "$otp_sha256sum $otp_archive_name" | sha256sum --check --status
-tar --extract --gunzip --directory=/tmp/$otp_directory_name < /tmp/$otp_archive_name
+tar --extract --gunzip --directory=$otp_directory_name < $otp_archive_name
 
-# 2.2 Download PDFium
+# 3. Download PDFium
 
-wget --quiet --directory-prefix /tmp $pdfium_download_link
+wget --quiet $pdfium_download_link
 echo "$pdfium_sha256sum $pdfium_archive_name" | sha256sum --check --status
-tar --extract --gunzip --directory=/tmp/$pdfium_directory_name < /tmp/$pdfium_archive_name
-
-popd
+tar --extract --gunzip --directory=$pdfium_directory_name < $pdfium_archive_name
 
 # 3. Compile
 
@@ -65,11 +57,11 @@ gcc \
   -Wno-unused-parameter \
   -Wmissing-prototypes \
   --std=c11 \
-  --include-directory /tmp/$otp_directory_name/usr/include \
-  --include-directory /tmp/$pdfium_directory_name/include \
+  --include-directory $otp_directory_name/usr/include \
+  --include-directory $pdfium_directory_name/include \
   --compile \
   --output=pdfium_nif.o \
-  ../c_src/pdfium_nif.c
+  /Users/eugene/projects/pdfium/c_src/pdfium_nif.c
 
 gcc \
   -arch $arch \
@@ -77,26 +69,21 @@ gcc \
   -undefined dynamic_lookup \
   -install_name @rpath/pdfium_nif.so \
   -l pdfium \
-  --library-directory /tmp/$otp_directory_name/usr/lib \
-  --library-directory /tmp/$pdfium_directory_name/lib \
-  --output /tmp/$output_directory_name/pdfium_nif.so \
+  --library-directory $otp_directory_name/usr/lib \
+  --library-directory $pdfium_directory_name/lib \
+  --output $output_directory_name/pdfium_nif.so \
   pdfium_nif.o
 
 # 3. Package
 
-cp /tmp/$pdfium_directory_name/lib/libpdfium.dylib /tmp/$output_directory_name
-
-pushd /tmp/$output_directory_name
-
-install_name_tool -change "./libpdfium.dylib" "@rpath/libpdfium.dylib" pdfium_nif.so
-install_name_tool -add_rpath "@loader_path" pdfium_nif.so
+install_name_tool -change "./libpdfium.dylib" "@rpath/libpdfium.dylib" $output_directory_name/pdfium_nif.so
+install_name_tool -add_rpath "@loader_path" $output_directory_name/pdfium_nif.so
 
 # 4. Create archive
-tar --create --gzip -s '|.*/||' --file /tmp/$output_directory_name/$output_name /tmp/$output_directory_name
+tar -cvf $output_name -s '|.*/||' $output_directory_name/pdfium_nif.so $pdfium_directory_name/lib/libpdfium.dylib
 
-popd
+echo $output_name
 
-ls /tmp/$output_directory_name
-otool -L /tmp/$output_directory_name/pdfium_nif.so
+# 5. Test
 
-elixir test.exs
+# tar -xvf $output_name -C $(pwd)
