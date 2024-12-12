7c6f00df1f18d3b142916472e78e86e3740b387e
Ievgen Pyrogov <iamexile@gmail.com>
2024-12-12 11:33:54 +0100

    yo

---
+++ checksum.exs
@@ -0,0 +1,4 @@
+%{
+  "pdfium-nif-2.17-aarch64-apple-darwin-0.1.0.tar.gz" => "sha256:60c800fc2dcb251aa8778022125211421a8fb5399306f95f2be42939adbfddd9",
+  "pdfium-nif-2.17-x86_64-apple-darwin-0.1.0.tar.gz" => "sha256:4a36b85e8711d34a50e73783485f71131617e463a59cef70fea15cca32276e67",
+}
