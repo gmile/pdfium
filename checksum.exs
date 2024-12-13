94dd0b852f9e6e403d63c2bb09dd6f7efea44c69
Ievgen Pyrogov <iamexile@gmail.com>
2024-12-13 13:28:07 +0100

    Add checkum

--- checksum.exs
+++ checksum.exs
@@ -1,4 +1,6 @@
 %{
-  "pdfium-nif-2.17-aarch64-apple-darwin-0.1.0.tar.gz" => "sha256:532809fc4080b842014f66689208f5ab4a3fb0f89c5ef37383a4f7c38b25c1f7",
-  "pdfium-nif-2.17-x86_64-apple-darwin-0.1.0.tar.gz" => "sha256:cedc990a6d2a17047c8c3e0c4613619623aa280b52636f7e0dae77c97dd3ce57",
+  "pdfium-nif-2.17-aarch64-apple-darwin-0.1.0.tar.gz" => "sha256:290d520beba719a050978aa1ab83a19e25171158b4c649af3a6fb3afe1fceaab",
+  "pdfium-nif-2.17-aarch64-linux-gnu-0.1.0.tar.gz" => "sha256:f54168683b94f44abcb526fea464b3fa6a40530e75912b90d537a9bffacf7fda",
+  "pdfium-nif-2.17-x86_64-apple-darwin-0.1.0.tar.gz" => "sha256:bd2a209aea0c11d0cca9dd2e6f97b3ccada100907c7c09e8d42faa94844d9fbc",
+  "pdfium-nif-2.17-x86_64-linux-gnu-0.1.0.tar.gz" => "sha256:4169f12d0fc7cd662ff7d2ec6306feafc69b9fd1ace9fa851ca324619cfdde9f",
 }
