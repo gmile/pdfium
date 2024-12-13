284d4383b3ed4e722fe3fd53e58a5a452ed53dcf
Ievgen Pyrogov <iamexile@gmail.com>
2024-12-13 12:37:24 +0100

    See if we don't need this

--- mix.exs
+++ mix.exs
@@ -15,46 +15,6 @@ defmodule PDFium.MixProject do
       start_permanent: Mix.env() == :prod,
       package: package(),
       deps: deps(),
-    ] ++ make_precompiler()
-  end
-
-  def make_precompiler do
-    [
-      make_precompiler_nif_versions: [
-        versions: ["2.16", "2.17"]
-      ],
-      cc_precompiler: [
-        cleanup: "clean",
-        allow_missing_compiler: true,
-        compilers: %{
-          {:unix, :linux} => %{
-            "x86_64-linux-gnu" => "x86_64-linux-gnu-",
-            "aarch64-linux-gnu" => "aarch64-linux-gnu-",
-            "armv7l-linux-gnueabihf" => "arm-linux-gnueabihf-",
-            "x86_64-linux-musl" => "x86_64-linux-musl-",
-            "aarch64-linux-musl" => "aarch64-linux-musl-"
-          },
-          {:unix, :darwin} => %{
-            "x86_64-apple-darwin" => {
-              "gcc",
-              "g++",
-              "<%= cc %> -arch x86_64",
-              "<%= cxx %> -arch x86_64"
-            },
-            "aarch64-apple-darwin" => {
-              "gcc",
-              "g++",
-              "<%= cc %> -arch arm64",
-              "<%= cxx %> -arch arm64"
-            }
-          },
-          {:win32, :nt} => %{}
-        }
-      ],
-      make_precompiler: {:nif, CCPrecompiler},
-      make_precompiler_filename: "pdfium_nif",
-      make_precompiler_priv_paths: ~w"pdfium_nif.so libpdfium.so libpdfium.dylib",
-      make_precompiler_url: "https://github.com/gmile/pdfium/releases/download/v#{@version}/@{artefact_filename}"
     ]
   end
 
