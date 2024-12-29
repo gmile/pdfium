ed4e0f6bc6914cfc610f0b997a41df5639510264
Ievgen Pyrogov <iamexile@gmail.com>
2024-12-29 15:25:39 +0100

    comment

--- test-cron.fish
+++ test-cron.fish
@@ -1,7 +1,9 @@
 #!/usr/bin/env fish
 
+# make sure remotely there's no branches called
+
 git checkout stable
-git reset --hard origin/main
+git reset --hard main
 echo 1 > LIBPDFIUM_TAG
 git add LIBPDFIUM_TAG
 git commit --message "Our current version"
