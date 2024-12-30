90c89a23dd2a3003f90024a80b1bf2f32fc11a80
Ievgen Pyrogov <iamexile@gmail.com>
2024-12-30 16:28:49 +0100

    update-test-script

--- test-open-pr.fish
+++ test-open-pr.fish
@@ -6,7 +6,8 @@ git push origin main
 set value (date +%s)
 git checkout -b branch-$value
 echo "New value: $value\n" >> README.md
-git add README.md 
+echo -n (awk 'BEGIN{FS=OFS="."} {$NF+=1}1' VERSION) > VERSION;
+git add README.md VERSION
 git commit -m "Testing $value"
 git push origin branch-$value
 gh pr create --fill
