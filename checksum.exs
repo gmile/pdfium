8edf3734cebac9888554d0840585a2c04f901b75
Ievgen Pyrogov <iamexile@gmail.com>
2024-12-30 13:01:34 +0100

    mix elixir_make.checksum --all

--- checksum.exs
+++ checksum.exs
@@ -1,8 +1,8 @@
 %{
-  "pdfium-nif-2.17-aarch64-apple-darwin-0.1.0.tar.gz" => "sha256:290d520beba719a050978aa1ab83a19e25171158b4c649af3a6fb3afe1fceaab",
-  "pdfium-nif-2.17-aarch64-linux-gnu-0.1.0.tar.gz" => "sha256:f54168683b94f44abcb526fea464b3fa6a40530e75912b90d537a9bffacf7fda",
-  "pdfium-nif-2.17-aarch64-linux-musl-0.1.0.tar.gz" => "sha256:18ae9069fecc4f7fd20cd38836b01d1f7dbd09590dde2920d9fee193728d81b4",
-  "pdfium-nif-2.17-x86_64-apple-darwin-0.1.0.tar.gz" => "sha256:bd2a209aea0c11d0cca9dd2e6f97b3ccada100907c7c09e8d42faa94844d9fbc",
-  "pdfium-nif-2.17-x86_64-linux-gnu-0.1.0.tar.gz" => "sha256:4169f12d0fc7cd662ff7d2ec6306feafc69b9fd1ace9fa851ca324619cfdde9f",
-  "pdfium-nif-2.17-x86_64-linux-musl-0.1.0.tar.gz" => "sha256:0110b498c161d2aa422c3df55340d51e68bd53201d40de1ed43d3a63eba6d0bb",
+  "pdfium-nif-2.17-aarch64-apple-darwin-0.1.14.tar.gz" => "sha256:0db88a5098d9a12e443a1ce371507d13857212ae4dbb16d7dcadcfb82989dabd",
+  "pdfium-nif-2.17-aarch64-linux-gnu-0.1.14.tar.gz" => "sha256:f3dce0be15b2ff34aa636db0471546310b1dcca6e875f3fafa4a4ce005e7e5d5",
+  "pdfium-nif-2.17-aarch64-linux-musl-0.1.14.tar.gz" => "sha256:f9af50754e6ee3130dff6c756804c89e2cb38c7ef3b05dca13c09d55298e0aac",
+  "pdfium-nif-2.17-x86_64-apple-darwin-0.1.14.tar.gz" => "sha256:5fe82fb504a9fbf1c7034d1ee406c87bd5b34e28a03bccd5e1bcd324b32ff168",
+  "pdfium-nif-2.17-x86_64-linux-gnu-0.1.14.tar.gz" => "sha256:c2179e91eb6254973b5677a0783d899841e8225b3f865eb12d27a2a588e6483b",
+  "pdfium-nif-2.17-x86_64-linux-musl-0.1.14.tar.gz" => "sha256:05f9c395618905d96401a3814b9fe581d1fb71c7b60e90874a16970a3d95bed2",
 }
