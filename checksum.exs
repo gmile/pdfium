dac5d9b42a4275863d212ad6401d4e7c2d66c8f1
gmile <gmile@users.noreply.github.com>
2025-01-08 18:23:12 +0000

    Update checksums

--- checksum.exs
+++ checksum.exs
@@ -1,8 +1,8 @@
 %{
-  "pdfium-nif-2.17-aarch64-apple-darwin-0.1.22.tar.gz" => "sha256:ad1a572ed60eaa6b68e73840682170fb1ccc0240d71a21c7b66b827e942c8758",
-  "pdfium-nif-2.17-aarch64-linux-gnu-0.1.22.tar.gz" => "sha256:58749e0abc0d24f0b607e35b730e529dec9d39c4f607537b239b79bc4cb64a1d",
-  "pdfium-nif-2.17-aarch64-linux-musl-0.1.22.tar.gz" => "sha256:e4f15b7fba5030b597396f7328bab19fc752e3c419b200b4bb35ed512aa86f36",
-  "pdfium-nif-2.17-x86_64-apple-darwin-0.1.22.tar.gz" => "sha256:e27f6e48c773674dfa2981fe7991ee9d07ea53c05129ca2a752ad54bdad258b7",
-  "pdfium-nif-2.17-x86_64-linux-gnu-0.1.22.tar.gz" => "sha256:0e7d99f4bd5ce6338a6dc711c6bce52cb37add7aea36e57035bdfff1bb1e7deb",
-  "pdfium-nif-2.17-x86_64-linux-musl-0.1.22.tar.gz" => "sha256:f92c2d118e8e1a2ce97069400430bfeca77437003ea0d0798d5fc91fa0c6ccc7"
+  "pdfium-nif-2.17-aarch64-apple-darwin-0.1.23.tar.gz" => "sha256:97daa8e1ec5266f244548f7367ad1e166e895296532d9d7ce6450353033944a4",
+  "pdfium-nif-2.17-aarch64-linux-gnu-0.1.23.tar.gz" => "sha256:b46300f58c47f436f6fb2a7fa9ed683825c55ea365a72446915175cd24e7b50a",
+  "pdfium-nif-2.17-aarch64-linux-musl-0.1.23.tar.gz" => "sha256:d3e1baa3a67259ccc193eee7704fcabeff6053b628abd371b567a87b9076c841",
+  "pdfium-nif-2.17-x86_64-apple-darwin-0.1.23.tar.gz" => "sha256:4fc33e20800e584d5f53f14c628d635ba482e3b33eeb23db3e86daa54f21fb79",
+  "pdfium-nif-2.17-x86_64-linux-gnu-0.1.23.tar.gz" => "sha256:aeac4d703437d323c784f1654b4958c852e5b425426f893ee374aed6e71a84c3",
+  "pdfium-nif-2.17-x86_64-linux-musl-0.1.23.tar.gz" => "sha256:4a08b5bb38ec75c391f7ba594ad0c9b131e7cccd89ce605c1c7422b7758c8c7a"
 }
